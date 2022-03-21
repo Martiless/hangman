837adb60dbef6ced26b4a8f18a537a689be82f08
Martina Martin <marti.law@gmail.com>
2022-03-21 11:38:45 +0000

    Add inital functions for game

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+
+print('Welcome to this Hangman game')
+
+def welcome_message()
+
+def pick_word()
+
+def start_game()
+
+def end_game()
