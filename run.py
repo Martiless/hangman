0d7b001430074858e2de143fde70f6b3372ca683
Martina Martin <marti.law@gmail.com>
2022-03-22 09:44:37 +0000

    Imported os to run.py file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import os 
 
 words = ['python', 'cobra', 'tiger', 'river', 'apple', 'avocado', 'mango', 'green', 'bridge', 'field', 'backpack', 'moon', 'planets', 'saturn', 'venus', 'sunset']
  
