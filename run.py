dba43800d8fdaed124678f7f1f94a30b35f0165a
Martina Martin <marti.law@gmail.com>
2022-03-22 10:22:15 +0000

    Add initial code to welcome message function

--- run.py
+++ run.py
@@ -1,14 +1,26 @@
 import random
 import os 
 
-words = ['python', 'cobra', 'tiger', 'river', 'apple', 'avocado', 'mango', 'green', 'bridge', 'field', 'backpack', 'moon', 'planets', 'saturn', 'venus', 'sunset']
- 
-print('Welcome to this Hangman game')
 
-def welcome_message()
+def welcome_message():
+    """ Welcome message for users"""
+    name = input('Please enter your name\n')
+    print(f'Welcome {name} to your hangman game\n')
+    print("GAME RULES:\n")
+    print('Your guess has to be a valid letter\n')
+    print('You will have 7 attemps to guess the correct letters\n')
+    print('Each correct letter guessed will appear in the answer area\n')
+    print('You will be notified if you guess a letter you already guessed\n')
+    print('NOW! LETS PLAY THE GAME!')
 
-def pick_word()
 
-def start_game()
 
-def end_game()
+def pick_word():
+    with open('words.txt') as f:
+        words = f.readlines()
+        return words
+
+
+#def start_game()
+
+#def end_game()
