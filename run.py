a467dd503b401f2e65ab8ea29a736ea9fb027935
Martina Martin <marti.law@gmail.com>
2022-03-24 09:43:31 +0000

    Correct styling errors

--- run.py
+++ run.py
@@ -1,35 +1,41 @@
 import random
 import os
 from words import words
-import time 
-#from enum import Enum
+# import time
+#  from enum import Enum
 
 # class Colors(Enum):
-#RED = 1
-#GREEN = 2
-#BLUE = 3
-#YELLOW = 4
-#PURPLE = 5
-
-#print(f'{Colors.PURPLE} This is purple, {Colors.RED} This is red, {Colors.BLUE} This is blue, {Colors.YELLOW} this is yellow and {Colors.GREEN} this is green')
+# RED = 1
+# GREEN = 2
+# BLUE = 3
+# YELLOW = 4
+# PURPLE = 5
 
 
-def clearConsole():
-    """ 
+def clear_console():
+    """
     Checks to see what os if the computer is running Windows
-    This piece of code was taken from www.delfstack.com"""
+    This piece of code was taken from www.delfstack.com
+    """
     command = 'clear'
     if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
         command = 'cls'
     os.system(command)
 
+
 def clear_screen():
-    """ Clears the screen after the instructions and if the user restarts the game"""
+    """
+    Clears the screen after the instructions
+    and if the user restarts the game
+    """
     os.system('cls')
-    clearConsole()
+    clear_console()
+
 
 def welcome_message():
-    """ Welcome message for users"""
+    """
+    Welcome message for users
+    """
     print('Welcome to Hangman\n')
     print("GAME RULES:\n")
     print('Your guess has to be a valid letter')
@@ -41,60 +47,65 @@ def welcome_message():
 
 
 def pick_word():
-    """This will randomly pick a word from a list of words"""
+    """
+    This will randomly pick a word from a list of words
+    """
     word = random.choice(words)
     return word.lower()  # will return all words in lower case
 
+
 def start_game():
     """
-    This starts the game once the user has entered a name 
+    This starts the game once the user has entered a name
     """
-    #clears the inital instructions from the screen once the game starts
+    # clears the inital instructions from the screen once the game starts
     clear_screen()
     word = pick_word()
-    #creates area of underscores that equals lenght of the word
+    # creates area of underscores that equals lenght of the word
     answer_area = "_" * len(word)
-    #empty list to add the guessed letters to once they are inputted 
+    # empty list to add the guessed letters to once they are inputted
     letters_guessed = []
     correct_answer = False
     lives = 7
     print('Lets start the game\n')
     print(answer_area)
 
-    #if lives is more than 0 and correct_answer is false the loop will keep running
+    # if lives is more than 0 and correct_answer is false
+    # the loop will keep running
     while lives > 0 and not correct_answer:
+        guesses = input('Please guess a letter\n')
+        # checks if the guess is only 1 letter and is in the alphabet
+        if len(guesses) == 1 and guesses.isalpha():
+            if guesses in word:
+                print(f'Woohoo {guesses} is correct.')
+                # add the guess to a list of letters
+                letters_guessed.append(guesses)
+                answer_area = guesses.join(answer_area)
+            if guesses in letters_guessed:
+                print(f'You have already guessed {guesses}, try again!')
+            else:
+                # takes away a life for every wrong guess
+                lives = lives - 1
+                print(f"""Sorry, {guesses} isn't correct.
+                You have {lives} lives left. Guess again""")
+                letters_guessed.append(guesses)
 
-     guesses = input('Please guess a letter\n')
-     #checks if the guess is only 1 letter and is in the alphabet
-     if len(guesses) == 1 and guesses.isalpha():
-         if guesses in word:
-             print(f'Woohoo {guesses} is correct.')
-             #add the guess to a list of letters
-             letters_guessed.append(guesses)
-             answer_area = guesses.join(answer_area)
-         if guesses in letters_guessed:
-             print(f'You have already guessed {guesses}, try again!')
-         else:
-             #takes away a life for every wrong guess
-             lives = lives - 1 
-             print(f"Sorry, {guesses} isn't correct. You have {lives} lives left. Guess again")
-             letters_guessed.append(guesses)
+        else:
+            print('Not a valid letter. Try again :)')
+        print(answer_area)
 
-     else:
-         print('Not a valid letter. Try again :)')
-     print(answer_area)
-
-    if correct_answer == True:
+    if correct_answer is True:
         print('Woohoo you guessed the word! Winner!!')
     else:
-        print(f'You are out of lives, sorry about that. The word was {word}. Better luck next time!')
-     
+        print(f"""You are out of lives, sorry about that.
+        The word was {word}. Better luck next time!""")
 
 
 def play():
     """
     Asked the user if they would like to play the game and calls
-    start_game if they answer yes"""
+    start_game if they answer yes
+    """
     game = input('Please enter Y or N \n')
     if game == 'Y':
         start_game()
@@ -105,12 +116,15 @@ def play():
         play()
 
 
-
 # def end_game():
 
+
 def main():
+    """
+    The main function that calls all other functions
+    """
     welcome_message()
     play()
 
-main()
 
+main()
